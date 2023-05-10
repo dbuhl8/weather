cdc3c97c575661b60d6827dd22f4209214d5a075
Dante Buhl <dbuhl@ucsc.edu>
2023-05-10 16:21:16 -0700

    Update weatherJS.js

--- js/weatherJS.js
+++ js/weatherJS.js
@@ -1,5 +1,4 @@
 
-
 let latitude = 0;
 let longitude = 0;
 const btn = document.getElementById("getWeatherBtn")
@@ -31,3 +30,30 @@ function success(position){
     longitude = position.coords.longitude;
     console.log(longitude + " " + latitude)
 }
+
+// const xhr = new XMLHttpRequest
+//  xhr.open("GET", "URL");
+//  xhr.send();
+//
+//  xhr.onload() =  function() {
+//    const body = JSON.parse(xhr.responseText);
+//    let temperature = body.temperature;
+//    let weatherStatus = body.weatherStatus;
+//    document.getElementbyId("temperature").innerHTML = `Temperature: ${temperature}`
+//    document.getElementbyId("weatherstatus").innerHTML = `Weather Status: ${weatherStatus}`
+//    vim rules mf!!!!!!
+//  }
+//
+// const xhr2 = new XHMLHttpRequest;
+//  xhr2.open("GET", `http://localhost:3000/weather/${lat}/${lon}');
+//  xhr2.send();
+//  
+//  xhr2.onload() = function() {
+//      const body = JSON.parse(xhr2.responseText);
+//      let forecast = body.forecast
+//  }
+//
+//  let helloString = `In it is degrees`
+//  let temp = 59
+//  let city = 'Santa Cruz'
+//  let finalString = `In ${city} it is ${temp} degrees`
